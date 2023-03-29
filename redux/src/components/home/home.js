be34159789b3171153af4db50049e2dc5a828c32
himanshu <sharmah1322@gmail.com>
2023-03-29 17:24:14 +0530

    NAME CHANGED

--- redux/src/components/home/home.js
+++ redux/src/components/home/home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <div id="post">
             <div id="post_header">
               <div id="dp"></div>
-              <div id="name">Himanshu Tyagi<img id="blue" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Eo_circle_light-blue_checkmark.svg/640px-Eo_circle_light-blue_checkmark.svg.png'></img><br/><span>Admin</span></div>
+              <div id="name">Himanshu Sharma<img id="blue" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Eo_circle_light-blue_checkmark.svg/640px-Eo_circle_light-blue_checkmark.svg.png'></img><br/><span>Admin</span></div>
             </div>
             <div id="post_content">
                 <img id="img" src='https://media.licdn.com/dms/image/D4D03AQFSXt5X9BfN5Q/profile-displayphoto-shrink_800_800/0/1678370042568?e=1685577600&v=beta&t=7iEg1J__sU_i1fmUTywSzA7Na7EJwo6exT-urjidKV0'></img>
