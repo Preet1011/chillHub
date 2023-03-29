df05c5706dd616b33cd054b2ab0d2c78741d0a78
Preet Tyagi <preet0896.be20@chitkara.edu.in>
2023-03-29 12:28:42 +0530

    pic change

--- redux/src/components/home/home.js
+++ redux/src/components/home/home.js
@@ -9,7 +9,7 @@ export default function Home() {
               <div id="name">Montu Garg<img id="blue" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Eo_circle_light-blue_checkmark.svg/640px-Eo_circle_light-blue_checkmark.svg.png'></img><br/><span>Admin</span></div>
             </div>
             <div id="post_content">
-                <img id="img" src='https://media.licdn.com/dms/image/D4D03AQFXQYX23QGp-A/profile-displayphoto-shrink_200_200/0/1668580690175?e=1683158400&v=beta&t=skZJLrmzl1TQo7XoN8Hdabu5wBouWKKz3F7h90IzNzI'></img>
+                <img id="img" src='https://media.licdn.com/dms/image/D4D03AQHAk5ebHt7UGA/profile-displayphoto-shrink_800_800/0/1677352595179?e=2147483647&v=beta&t=0vblM4UkoUQePymhqF4wG4hGf6lf0zohS6iRLrKDqlg'></img>
             </div>
             <div id="post_footer">
                 <span>like</span>
