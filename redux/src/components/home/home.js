f024ffe1f43105e2f3f3b93708476b66e71f9cda
unknown <paras0892.be20@chitkara.edu.in>
2023-03-29 17:19:23 +0530

    Himanshu Tyagi

--- redux/src/components/home/home.js
+++ redux/src/components/home/home.js
@@ -6,10 +6,10 @@ export default function Home() {
         <div id="post">
             <div id="post_header">
               <div id="dp"></div>
-              <div id="name">Montu Garg<img id="blue" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Eo_circle_light-blue_checkmark.svg/640px-Eo_circle_light-blue_checkmark.svg.png'></img><br/><span>Admin</span></div>
+              <div id="name">Himanshu Tyagi<img id="blue" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Eo_circle_light-blue_checkmark.svg/640px-Eo_circle_light-blue_checkmark.svg.png'></img><br/><span>Admin</span></div>
             </div>
             <div id="post_content">
-                <img id="img" src='https://media.licdn.com/dms/image/D4D03AQHAk5ebHt7UGA/profile-displayphoto-shrink_800_800/0/1677352595179?e=2147483647&v=beta&t=0vblM4UkoUQePymhqF4wG4hGf6lf0zohS6iRLrKDqlg'></img>
+                <img id="img" src='https://media.licdn.com/dms/image/D4D03AQFSXt5X9BfN5Q/profile-displayphoto-shrink_800_800/0/1678370042568?e=1685577600&v=beta&t=7iEg1J__sU_i1fmUTywSzA7Na7EJwo6exT-urjidKV0'></img>
             </div>
             <div id="post_footer">
                 <span>like</span>
